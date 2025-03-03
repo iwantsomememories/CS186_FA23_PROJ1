3e96a3c6584cb3817511cba38f51d60724a0f773
fanqiechaodan <1484119655@qq.com>
2025-03-03 20:58:21 +0800

    Finish the task three and four of PROJ 1

--- proj1.sql
+++ proj1.sql
@@ -89,46 +89,116 @@ AS
 -- Question 3i
 CREATE VIEW q3i(playerid, namefirst, namelast, yearid, slg)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT p.playerID, p.namefirst, p.namelast, b.yearID, ((b.H + b.H2B + 2 * b.H3B + 3 * b.HR) * 1.0 / b.AB) AS slg
+  FROM batting as b INNER JOIN people as p ON b.playerID = p.playerID
+  WHERE b.AB > 50
+  ORDER BY slg DESC, b.yearID ASC, p.playerID ASC
+  LIMIT 10
 ;
 
 -- Question 3ii
 CREATE VIEW q3ii(playerid, namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT b.playerID, p.namefirst, p.namelast, ((SUM(b.H) + SUM(b.H2B) + 2 * SUM(b.H3B) + 3 * SUM(b.HR)) * 1.0 / SUM(b.AB)) AS lslg
+  FROM batting as b INNER JOIN people as p ON b.playerID = p.playerID
+  GROUP BY b.playerID
+  HAVING SUM(b.AB) > 50
+  ORDER BY lslg DESC, b.playerID ASC
+  LIMIT 10
 ;
 
 -- Question 3iii
 CREATE VIEW q3iii(namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT p.namefirst, p.namelast, ((SUM(b.H) + SUM(b.H2B) + 2 * SUM(b.H3B) + 3 * SUM(b.HR)) * 1.0 / SUM(b.AB)) AS lslg
+  FROM batting as b INNER JOIN people as p ON b.playerID = p.playerID
+  GROUP BY b.playerID
+  HAVING SUM(b.AB) > 50 AND lslg > (SELECT ((SUM(b.H) + SUM(b.H2B) + 2 * SUM(b.H3B) + 3 * SUM(b.HR)) * 1.0 / SUM(b.AB)) FROM batting as b WHERE b.playerID = "mayswi01")
 ;
 
 -- Question 4i
 CREATE VIEW q4i(yearid, min, max, avg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT yearID, MIN(salary), MAX(salary), AVG(salary)
+  FROM salaries
+  GROUP BY yearID
+  ORDER BY yearID ASC
 ;
 
 -- Question 4ii
 CREATE VIEW q4ii(binid, low, high, count)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  WITH s2016 AS (
+    SELECT * FROM salaries
+    WHERE yearID = 2016
+  ),
+  sstats AS (
+    SELECT MIN(salary) as mins, MAX(salary) as maxs, (MAX(salary) - MIN(salary)) / 10.0 AS width
+    FROM s2016
+  ),
+  sbinids AS (
+    SELECT s2016.salary, MIN(CAST((s2016.salary - sstats.mins) / sstats.width AS INT), 9) AS binid
+    FROM s2016, sstats
+  )
+  SELECT b.binid, (SELECT mins FROM sstats) + b.binid * (SELECT width FROM sstats), (SELECT mins FROM sstats) + (b.binid + 1) * (SELECT width FROM sstats), COUNT(*)
+  FROM binids AS b LEFT JOIN sbinids AS s ON b.binid = s.binid 
+  GROUP BY b.binid
 ;
 
 -- Question 4iii
 CREATE VIEW q4iii(yearid, mindiff, maxdiff, avgdiff)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  WITH sstats(yearID, mins, maxs, avgs) AS (
+    SELECT yearID, MIN(salary), MAX(salary), AVG(salary)
+    FROM salaries
+    GROUP BY yearID
+  )
+  SELECT a.yearID, a.mins - b.mins, a.maxs - b.maxs, a.avgs - b.avgs
+  FROM sstats AS a INNER JOIN sstats AS b ON a.yearID = b.yearID + 1
+  ORDER BY a.yearID ASC
 ;
 
 -- Question 4iv
 CREATE VIEW q4iv(playerid, namefirst, namelast, salary, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  WITH salaries_of_twoyears AS (
+    SELECT * 
+    FROM salaries
+    WHERE yearID = 2000 OR yearID = 2001
+  ),
+  max_salary(yearID, peak) AS (
+    SELECT yearID, MAX(salary)
+    FROM salaries_of_twoyears
+    GROUP BY yearID
+  )
+  SELECT s.playerID, p.namefirst, p.namelast, s.salary, s.yearID
+  FROM salaries_of_twoyears AS s 
+  INNER JOIN people AS p ON s.playerID = p.playerID
+  INNER JOIN max_salary AS m ON s.yearID = m.yearID
+  WHERE s.salary = m.peak
+  ORDER BY s.yearID ASC
 ;
 -- Question 4v
 CREATE VIEW q4v(team, diffAvg) AS
-  SELECT 1, 1 -- replace this line
+  WITH team_2016 AS (
+    SELECT * 
+    FROM teams
+    WHERE yearID = 2016
+  ),
+  salary_2016 AS (
+    SELECT *
+    FROM salaries
+    WHERE yearID = 2016
+  ),
+  allstar_2016 AS (
+    SELECT *
+    FROM allstarfull
+    WHERE yearID = 2016
+  )
+  SELECT t.teamID, (MAX(s.salary) - MIN(s.salary))
+  FROM team_2016 AS t 
+  INNER JOIN allstar_2016 AS a ON t.teamID = a.teamID
+  INNER JOIN salary_2016 AS s ON a.playerID = s.playerID
+  GROUP BY t.teamID
 ;
 
